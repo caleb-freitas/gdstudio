e4ad564fd03ca1efe762610947b4e6716bc0c4e5
caleb-freitas <calebfreitas@tutanota.com>
2022-10-26 17:08:04 -0300

    feat: added new colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,46 @@ module.exports = {
 		"./node_modules/flowbite/**/*.js"
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"transparent": "transparent",
+				"current": "currentColor",
+
+				light: {
+					green: {
+						100: "",
+						700: "#272f27",
+						900: "#2c3e2d"
+					},
+					gray: {
+						100: "",
+						300: "#eeeff1",
+						500: "#272f27"
+					}
+				},
+				dark: {
+					gray: {
+						900: "hsl(0, 0%, 2%)"
+					}
+				}
+
+				// --theme-primary: ;
+				// --theme-primary-hover: "#395a3b";
+				// --theme-on-primary: hsl(0, 0%, 100%);
+
+				// --theme-bg: ;
+				// --theme-on-bg: ;
+
+				// --theme-surface-1: #e6e4e0;
+				// --theme-on-surface-1: #272f27;
+
+				// --theme-surface-2: #c3d9c4;
+				// --theme-on-surface-2: #000;
+
+			}
+		},
 	},
 	plugins: [require('flowbite/plugin')],
 	darkMode: "class"
 }
+
