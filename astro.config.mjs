2fd68b3e3843cce98e2ca68fcd01ce66a3ef3580
caleb-freitas <calebfreitas@tutanota.com>
2022-12-08 17:29:25 -0300

    chore: move css directives to its own file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -9,9 +9,11 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind(),
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
     image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    })
-  ]
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
